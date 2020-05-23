62db2d0e36f6196b51c44862c970ad8c31df45a3
Kuro-Games <kuro@kuro.games>
2020-05-23 17:09:06 +0900

    簡単なモックアップ制作 ホーム画面まで
    Signed-off-by: Kuro-Games <kuro@kuro.games>

---
+++ Assets/Scripts/k.tamura/Manager/Mock/MockSceneMgr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MockSceneMgr : SingletonMonoBehaviour<MockSceneMgr>
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
---
+++ Assets/Scripts/k.tamura/Manager/Mock/MockTitleMgr.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockTitleMgr : MonoBehaviour
+{
+    public void StartPush()
+    {
+        MockSceneMgr.LoadScene("MockHome");
+    }
+}
