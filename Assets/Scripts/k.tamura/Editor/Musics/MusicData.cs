e58be7427eb2ac6bdae7b318efaff66cff2da611
Kuro-Games <kuro@kuro.games>
2020-05-27 04:50:32 +0900

    曲データScriptableObject修正,とりあえず完成

--- Assets/Scripts/k.tamura/Editor/Musics/MusicData.cs
+++ Assets/Scripts/k.tamura/Editor/Musics/MusicData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 public class MusicData : EditorWindow
 {
@@ -11,7 +12,6 @@ public class MusicData : EditorWindow
     string Composer;
     string NortsDesigner;
     string FilePath;
-    string[] FileFilter = {"asset","*"}; 
     [MenuItem("SingForAI-Tools/Music/DataCreate")]
     private static void Open()
     {
@@ -23,62 +23,86 @@ public class MusicData : EditorWindow
     private void OnGUI()
     {
         EditorGUILayout.LabelField("曲のデータを作るところ");
-        GUILayout.Label("MusicName");
-        MName = GUILayout.TextArea(MName);
-        //msettings.MusicName = MName;
-        GUILayout.Label("MusicBPM");
-        MBPM = GUILayout.TextArea(MBPM);
-        //msettings.MusicBPM = float.Parse(MBPM);
-        GUILayout.Label("Composer");
-        Composer = GUILayout.TextArea(Composer);
-        //msettings.Composer = Composer;
-        GUILayout.Label("NortsDesignerName");
-        NortsDesigner = GUILayout.TextArea(NortsDesigner);
 
-        //msettings.NortsDesigner = NortsDesigner;
 
+        EditorGUI.BeginChangeCheck();
+        if (msettings != null)
+        {
+            GUILayout.Label("ファイル名");
+            GUILayout.Label(FilePath);
+            GUILayout.Label("MusicName");
+            MName= GUILayout.TextArea(msettings.MusicName);
+            GUILayout.Label("MusicBPM");
+            MBPM = GUILayout.TextArea(msettings.MusicBPM.ToString());
+            GUILayout.Label("Composer");
+            Composer = GUILayout.TextArea(msettings.Composer);
+            GUILayout.Label("NortsDesignerName");
+            NortsDesigner = GUILayout.TextArea(msettings.NortsDesigner);
+        }
+
+        if (EditorGUI.EndChangeCheck())// 変更を検知した場合、設定ファイルに戻す
+        { 
+            UnityEditor.Undo.RecordObject(msettings, "Edit ExampleEditorWindow"); 
+            msettings.MusicName = MName;
+            msettings.MusicBPM = float.Parse(MBPM);
+            msettings.Composer = Composer;
+            msettings.NortsDesigner = NortsDesigner;
+            EditorUtility.SetDirty(msettings);
+        }
         if (GUILayout.Button("Load"))
         {
             Debug.Log("Button!");
             Load();
         }
-        if (GUILayout.Button("Save"))
-        {
-            Debug.Log("Button!");
-            Save(FilePath);
-
-        }
-        if (GUILayout.Button("Create"))
+        if (GUILayout.Button("NewCreate"))
         {
             Debug.Log("Button!");
             Create();
-
         }
     }
     private void Load()
     {
-        FilePath = EditorUtility.OpenFilePanelWithFilters("曲データをロードする","", FileFilter);
+        FilePath = EditorUtility.OpenFilePanel("曲データをロードする","","");
+        string[] FilePaths = FilePath.Split('/');
+        FilePath = "";
+        int index = Array.IndexOf(FilePaths, "Assets");
+        Debug.Log(index);
+        for (; index < FilePaths.Length; index++)
+        {
+
+            FilePath += FilePaths[index] ;
+            if (index<FilePaths.Length-1)
+            {
+                FilePath += "/";
+            }
+        }
+        Debug.Log(FilePath);
         msettings = AssetDatabase.LoadAssetAtPath<MusicDataSettings>(FilePath);
+        Repaint();
     }
     private void Save(string FPath)
     {
         if(FPath.Length != 0)
         {
-
+            msettings.MusicName = MName;
+            msettings.MusicBPM = float.Parse(MBPM);
+            msettings.Composer = Composer;
+            msettings.NortsDesigner = NortsDesigner;
+            EditorUtility.SetDirty(msettings);
+            Repaint();
             return;
-        }
-        FilePath = EditorUtility.SaveFilePanelInProject("曲データをセーブする", MName, "asset", ".assetでセーブしてください。");
-        if (FilePath.Length != 0)
-        {
-            AssetDatabase.CreateAsset(msettings, FilePath);
         }
     }
     private void Create()
     {
-        FilePath = EditorUtility.SaveFilePanelInProject("曲データをセーブする", MName, "asset", ".assetでセーブしてください。");
+        FilePath = EditorUtility.SaveFilePanelInProject("曲データをセーブする", "musicName", "asset", ".assetでセーブしてください。");
         if (FilePath.Length != 0)
         {
-            AssetDatabase.CreateAsset(msettings, FilePath);
+            AssetDatabase.CreateAsset(CreateInstance <MusicDataSettings>(), FilePath);
         }
+    }
+    private void Update()
+    {
+        Repaint();
     }
 }
--- Assets/Scripts/k.tamura/Editor/Musics/MusicDataSettings.cs
+++ Assets/Scripts/k.tamura/Editor/Musics/MusicDataSettings.cs
@@ -8,5 +8,25 @@ public class MusicDataSettings : ScriptableObject
     public float MusicBPM;
     public string Composer;
     public string NortsDesigner;
+
+	public string GetMusicName()
+	{
+		return MusicName;
+	}
+
+	public float GetMusicBPM()
+	{
+		return MusicBPM;
+	}
+
+	public string GetComposer()
+	{
+		return Composer;
+	}
+
+	public string GetNortsDesigner()
+	{
+		return NortsDesigner;
+	}
 }
 
