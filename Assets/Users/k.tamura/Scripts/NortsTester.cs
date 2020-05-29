bb65df43960b124955ae7994d691ec81cdef6fde
Kuro-Games <kuro@kuro.games>
2020-05-29 14:44:19 +0900

    BPMテスト用スクリプト一度Up

--- Assets/Users/k.tamura/Scripts/NortsTester.cs
+++ Assets/Users/k.tamura/Scripts/NortsTester.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class NortsTester : MonoBehaviour
 {
+    [SerializeField]
+    float positiony=0.1f;
     private void Update()
     {
-        
+        this.transform.position -= new Vector3(0, positiony, 0);
     }
 }
