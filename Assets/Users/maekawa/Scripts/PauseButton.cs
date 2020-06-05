9c806653f7b878ee24bdfb73d3df5c971c180466
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-06-05 17:00:48 +0900

    ポーズ画面途中

---
+++ Assets/Users/maekawa/Scripts/PauseButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject PauseUI;
+
+	// Start is called before the first frame update
+	public void OnClick()
+	{
+		PauseUI.SetActive(!PauseUI.activeSelf);
+
+		//　ポーズUIが表示されてる時は停止
+		if (PauseUI.activeSelf)
+		{
+			Time.timeScale = 0f;
+			//　ポーズUIが表示されてなければ通常通り進行
+		}
+		else
+		{
+			Time.timeScale = 1f;
+		}
+	}
+}
