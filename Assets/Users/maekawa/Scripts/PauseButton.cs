3ae0e7203c8eb47ce99c36089632aabbf6214fef
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-06-05 17:44:38 +0900

    ポーズ画面完成

--- Assets/Users/maekawa/Scripts/PauseButton.cs
+++ Assets/Users/maekawa/Scripts/PauseButton.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 
 public class PauseButton : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject PauseUI;
+	//対象のオブジェクト（今回はPauseUI）をアタッチする必要あり
+	//手順：スクリプトをアタッチ→インスペクターで対象をアタッチ
 
-	// Start is called before the first frame update
+	[SerializeField] //エディタ拡張
+	private GameObject PauseUI; //ポーズ時に表示されるUI
+
+	//ButtonインスペクターのOnClick()にButtonを追加→Buttonをアタッチ→実行させたい関数を指定
+
 	public void OnClick()
 	{
+		//ポーズUIのアクティブ、非アクティブを切り替え
 		PauseUI.SetActive(!PauseUI.activeSelf);
 
 		//　ポーズUIが表示されてる時は停止
 		if (PauseUI.activeSelf)
 		{
 			Time.timeScale = 0f;
-			//　ポーズUIが表示されてなければ通常通り進行
 		}
+		//　ポーズUIが表示されてなければ通常通り進行
 		else
 		{
 			Time.timeScale = 1f;
