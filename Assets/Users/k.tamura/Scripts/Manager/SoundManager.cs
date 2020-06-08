2956b20c365598280b0d54b1e9e9242b95e098b8
Kuro-Games <kuro@kuro.games>
2020-06-08 22:21:18 +0900

    サウンドマネージャー作成完了 今後の仕様次第で機能追加します。

---
+++ Assets/Users/k.tamura/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SoundManager:SingletonMonoBehaviour<SoundManager>
+{
+    [SerializeField]
+    CriAtomSource BGMSource = null;
+    [SerializeField]
+    CriAtomSource SESource = null;
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public static void BGMSoundCue(int cueID)
+    {
+        Instance.BGMSource.Play(cueID);
+    }
+    public static void SESoundCue(int cueID)
+    {
+        Instance.SESource.Play(cueID);
+    }
+}
