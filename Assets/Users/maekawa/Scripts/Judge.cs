227cb080b32adbcbdd322bc933b1bd4e4a9bd999
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-06-12 17:57:26 +0900

    判定途中３

--- Assets/Users/maekawa/Scripts/Judge.cs
+++ Assets/Users/maekawa/Scripts/Judge.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Judge : MonoBehaviour
 {
-    int[] lane_arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+    [SerializeField] private int point = 1000;
+    [SerializeField] private float perfect = 15;
+    [SerializeField] private float great = 30;
+    [SerializeField] private float miss = 40;
+
     int score = 0;
-    [SerializeField] int point = 1000;
+    int combo = 0;
+    int[] laneArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+    //float judgeLinePosY = -316;
+    //float notePosY = -300;
+    GameObject white;
+    white.transform.position.y;
 
     void Start()
     {
@@ -18,9 +28,27 @@ public class Judge : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
             var pos = Input.mousePosition;
+            if ()//レーンが一致していれば
+            {
+                float tapTiming = //judgeLinePosY - notePosY;
+                float absTiming = Mathf.Abs(tapTiming);
 
-            score =+ point;
-            Debug.Log(score);
+                if (absTiming <= perfect)
+                {
+                    score =+ point;
+                    combo ++;
+                    Debug.Log(score);
+                }
+                else if(absTiming <= great)
+                {
+                    score =+ point / 2;
+                    combo ++;
+                }
+                else if(absTiming <= miss)
+                {
+                    combo = 0;
+                }
+            }
         }
     }
 }
