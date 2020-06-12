12c33941828582012d1492ffd6e4a2b224b9e357
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-06-12 13:59:38 +0900

    判定作成途中

---
+++ Assets/Users/maekawa/Scripts/Judge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Judge : MonoBehaviour
+{
+    int[] lane_arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+    int score = 0;
+    [SerializeField] int point = 1000;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            var pos = Input.mousePosition;
+
+            score =+ point;
+            Debug.Log(score);
+        }
+    }
+}
