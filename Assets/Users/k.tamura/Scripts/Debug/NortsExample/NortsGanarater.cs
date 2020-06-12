33370c1482132c6eb129d57fcd72eaf7a3e5ab34
Kuro-Games <kuro@kuro.games>
2020-06-12 17:59:29 +0900

    プログラム確認のため一時退避

--- Assets/Users/k.tamura/Scripts/Debug/NortsExample/NortsGanarater.cs
+++ Assets/Users/k.tamura/Scripts/Debug/NortsExample/NortsGanarater.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-
+using System;
+using System.IO;
 public class NortsGanarater : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField, Header("ノーツを生成する元(Prefab)")]
+    GameObject notes = null;
+    [SerializeField]
+    List<GameObject> NotesGen = null;
+    public static int Speed = 1000;
+
+    ///デバッグ用。最終的には消す！
+    private void Awake()
     {
-        
+        if (notes = null)
+        {
+            Debug.LogError("NotesGeneraterのnotesが指定されていません。");
+        }
+    }
+    public void ButtonPush()
+    {
+        FileInfo info = new FileInfo(Application.streamingAssetsPath + "/musics.nts");
+        StreamReader reader = new StreamReader(info.OpenRead());
+        string json = reader.ReadToEnd();
+        GenerateNotes(json);
+    }
+    public void GenerateNotes(string MusicDatas)
+    {
+        NotesJson musicData = LoadJson(MusicDatas);
+        Debug.Log(JsonUtility.ToJson(musicData));
+    }
+    private NotesJson LoadJson(string MusicStrings)
+    {
+        return JsonUtility.FromJson<NotesJson>(MusicStrings);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
+//歌詞を外部に頼んでる
+//
---
+++ Assets/Users/k.tamura/Scripts/Debug/NortsExample/NotesJson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+[Serializable]
+public struct NotesJson
+{
+    [Serializable]
+    public struct MusicData
+    {
+
+        public string characterCode;
+        public int miType;
+        public string musicName;
+        public int bpm;
+        public string composer;
+        public string notesCreator;
+        public double offset;
+        public List<Notes> notes;
+    }
+    [Serializable]
+    public struct Notes
+    {
+        public int type;
+        public int lane;
+        public int num;
+        public List<Notes> notes;
+    }
+}
+
