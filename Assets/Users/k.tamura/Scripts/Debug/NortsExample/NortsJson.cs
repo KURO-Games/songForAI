57fa36d22eab4154ce05974cead5db31fa407643
Kuro-Games <kuro@kuro.games>
2020-06-12 14:41:37 +0900

    ノーツ生成用スクリプト

---
+++ Assets/Users/k.tamura/Scripts/Debug/NortsExample/NortsJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class NortsJson
+{
+    public static string characterCode = "";
+    public static int miType = 0;
+    public static string musicName = "";
+    public static int bpm = 0;
+    public static string composer = "";
+    public static string notesCreator = "";
+    public static double offset = 0.0;
+
+
+}
