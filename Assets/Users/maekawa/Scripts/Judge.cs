02b544ff0fb4fc84e325b4ac5b6087c0a1931af9
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-06-24 01:38:37 +0900

    raycast

--- Assets/Users/maekawa/Scripts/Judge.cs
+++ Assets/Users/maekawa/Scripts/Judge.cs
@@ -11,45 +11,52 @@ public class Judge : MonoBehaviour
     [SerializeField] private float miss = 40;
     [SerializeField] private GameObject[] Lanes = new GameObject[8];
 
+    GameObject JudgeLine;
     int score = 0;
     int combo = 0;
     int life = 1000;
     float notePosY = -300; //仮
-    Image JudgeLine;
 
     float GapDistance() //距離を算出
     {
-        float tapTiming = JudgeLine.transform.position.y - notePosY; //ズレの値
+        // XXX エラーです
+        var io = JudgeLine.transform.position.y;//JudgeLineのy座標を取得
+        float tapTiming = io - notePosY; //ズレの値
         float absTiming = Mathf.Abs(tapTiming); //絶対値に変換
         return absTiming;
     }
 
     void Update() //判定
     {
-        var io = JudgeLine.transform.position.y;//Image JudgeLineのy座標を取得
-
         if (Input.GetMouseButtonDown(0))
         {
             Vector2 tapPos = Input.mousePosition;
-            if (tapPos = Lanes[i])//レーンが一致していれば
+
+            int layerMask = 1;
+            float maxDistance = 10;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance, layerMask);
+            if (hit.collider)
             {
                 float absTiming = GapDistance();
 
                 if (absTiming <= perfect)
                 {
-                    score =+ point;
-                    combo ++;
+                    score = +point;
+                    combo++;
                     Debug.Log(score);
                     Debug.Log(combo);
                 }
-                else if(absTiming <= great)
+                else if (absTiming <= great)
                 {
-                    score =+ point / 2;
-                    combo ++;
+                    score = +point / 2;
+                    combo++;
                     Debug.Log(score);
                     Debug.Log(combo);
                 }
-                else if(absTiming <= miss)
+                else if (absTiming <= miss)
                 {
                     combo = 0;
                 }
@@ -57,3 +64,32 @@ public class Judge : MonoBehaviour
         }
     }
 }
+
+//    if (Input.GetMouseButtonDown(0))
+//    {
+//        Vector2 tapPos = Input.mousePosition;
+//        if (tapPos = Lanes[i])//レーンが一致していれば
+//        {
+//            float absTiming = GapDistance();
+
+//            if (absTiming <= perfect)
+//            {
+//                score =+ point;
+//                combo ++;
+//                Debug.Log(score);
+//                Debug.Log(combo);
+//            }
+//            else if(absTiming <= great)
+//            {
+//                score =+ point / 2;
+//                combo ++;
+//                Debug.Log(score);
+//                Debug.Log(combo);
+//            }
+//            else if(absTiming <= miss)
+//            {
+//                combo = 0;
+//            }
+//        }
+//    }
+//}
