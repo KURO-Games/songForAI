4f51787421fede5765c123062cae1a713ad00107
kuro-games <kuro@kuro.games>
2020-06-26 13:49:54 +0900

    音ゲー部UI追加
    Signed-off-by: kuro-games <kuro@kuro.games>

---
+++ Assets/Users/k.tamura/Scripts/Debug/Mock/SelectMusicScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectMusicScene : MonoBehaviour
+{
+    string _name;
+    Button PushButton;
+    bool _isTap = false;
+    private void Start()
+    {
+        _isTap = false;
+    }
+    public void BackHome()
+    {
+        if (!_isTap)
+        {
+            _isTap = true;
+            SceneLoadManager.LoadScene("MockHome");
+        }
+    }
+    public void SelectMusic(Button _button)
+    {
+        if (!_isTap)
+        {
+            _isTap = true;
+            _name=_button.name;
+            SceneLoadManager.LoadScene("SelectMusic");
+        }
+    }
+
+}
