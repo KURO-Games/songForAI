bc7aa23808fc6c9d04c481508568490375f354db
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-06-26 18:49:04 +0900

    プラットフォーム、画面サイズなど変更

--- Assets/Users/maekawa/Scripts/PauseButton.cs
+++ Assets/Users/maekawa/Scripts/PauseButton.cs
@@ -11,12 +11,12 @@ public class PauseButton : MonoBehaviour
 		//ポーズUIのアクティブ、非アクティブを切り替え
 		PauseUI.SetActive(!PauseUI.activeSelf);
 
-		//　ポーズUIが表示されてる時は停止
+		//ポーズUIが表示されてる時は停止
 		if (PauseUI.activeSelf)
 		{
 			Time.timeScale = 0f;
 		}
-		//　ポーズUIが表示されてなければ通常通り進行
+		//ポーズUIが表示されてなければ通常通り進行
 		else
 		{
 			Time.timeScale = 1f;
