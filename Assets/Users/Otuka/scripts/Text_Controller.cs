0bb3844134eccb08aeb55582c8be76f78d921911
yuto otuka <akagekaga@gmail.com>
2020-07-17 16:03:55 +0900

    MessageControllerの作成中

--- Assets/Users/Otuka/scripts/Text_Controller.cs
+++ Assets/Users/Otuka/scripts/Text_Controller.cs
@@ -6,53 +6,91 @@ using System.IO;
 
 public class Text_Controller : MonoBehaviour
 {
+    //メッセージテキスト
     [SerializeField]
-    Text text;
-    string[] Cat_Text;
-    int Text_Num;
+    Text Message;
+    //名前テキスト
+    [SerializeField]
+    Text Name;
 
-    /// <summary>
-    /// テキストに区切りをつける
-    /// 順番にテキスト表示
-    /// 
-    /// </summary>
+    string[] Text_Message;  //一時的に１列のデータを格納する所
+    string[,] Text_Words;   //ID　Name　Messageに分けて格納すること
+
+    int Vertical_Num;   //縦列
+    int Side_Num;       //横列
+
+    int Display_Num;    //何番目の会話を表示してるか
+
+    [SerializeField]
+    float Message_Speed = 0;
 
 
-    
+
     private void Start()
     {
-        Text_Num = 0;
-        Text_Cat();
+        Display_Num = 0;
+        Text_Load();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Next_Text();
+            Message_Display();
         }
     }
 
-    void Load_txt()
+    #region テキストロード
+    void Text_Load()
     {
+        //txtからロード
+        TextAsset textAsset = new TextAsset();
+        textAsset = Resources.Load("Text_Test", typeof(TextAsset)) as TextAsset;
 
-    }
+        //ロードしたのを一度格納
+        string Text_Lines = textAsset.text;
 
-    void Text_Cat()
-    {
-        
-    }
+        //行ごとに分割
+        Text_Message = Text_Lines.Split('\n');
 
-    void Next_Text()
-    {
-        if (Text_Num < Cat_Text.Length)
+        //横列認識
+        Side_Num = Text_Message[0].Split('\t').Length;
+        //縦列認識
+        Vertical_Num = Text_Lines.Split('\n').Length;
+
+        //配列要素数確定させる
+        Text_Words = new string[Vertical_Num, Side_Num];
+
+        for(var i = 0; i < Vertical_Num; i++)
         {
-            text.text = Cat_Text[Text_Num];
-            Text_Num++;
+            //保存する行を確定、保存
+            string[] TempWords = Text_Message[i].Split('\t');
+
+            for (var n = 0; n < Side_Num; n++)
+            {
+                //保存する列を確定、保存
+                Text_Words[i, n] = TempWords[n];
+            }
         }
-        else
-        {
-            text.text = "次のシーンへ";
-        }
-        
     }
+    #endregion
+
+    #region メッセージ表示
+    void Message_Display()
+    {
+        //名前表示
+        Name.text = Text_Words[Display_Num, 1];
+
+        //メッセージ表示
+        //テキストリセット
+        Message.text = "";
+        int Message_Count = 0;
+        while (Text_Words[Display_Num, 2].Length > Message_Count)
+        {
+            Message.text += Text_Words[Display_Num, 2][Message_Count];
+            Message_Count++;
+            
+        }
+        Display_Num++;
+    }
+    #endregion
 }
