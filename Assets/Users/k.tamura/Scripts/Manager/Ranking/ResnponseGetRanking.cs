759e908309c43cfdb499247936d8834c9d7495f8
kuro-games <kuro@kuro.games>
2020-09-17 01:09:42 +0900

    ランキング系統のクラス作成

---
+++ Assets/Users/k.tamura/Scripts/Manager/Ranking/ResnponseGetRanking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class ResnponseGetRanking
+{
+    public List<Ranking> rankings;
+}
+
+[Serializable]
+public class Ranking
+{
+    public int id;
+    public string game_id;
+    public int rank;
+    public string name;
+    public string score;
+    public string comment;
+    public DateTime created_at;
+    public DateTime updated_at;
+}
