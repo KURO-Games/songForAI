2fed225461ce109025ff997f44698919dfa0495a
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2020-09-18 18:53:36 +0900

    jacket追加 csv読み込み完了

---
+++ Assets/Users/sakamaki/Scripts/Musicbutton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Musicbutton : MonoBehaviour
+{
+    [SerializeField]
+    private Text _musicName;
+    [SerializeField]
+    private Image _jacketImage;
+
+    public string musicName;
+    public string jacketImage;
+
+    public void Initialized()
+    {
+        _musicName.text = musicName;
+        _jacketImage.sprite = Resources.Load<Sprite>("Jacket/" + jacketImage);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Users/sakamaki/Scripts/readMusic.cs
+++ Assets/Users/sakamaki/Scripts/readMusic.cs
@@ -3,44 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Text;
+using UnityEditor.UIElements;
+using System;
 
 public class readMusic : MonoBehaviour
 {
     // 保管する用のListClass
-    private List<string[]> musicList = new List<string[]>();
-    // csvファイルの中身をutf形式でencodingする
-    private Encoding encoding;
+    // 配列を使う
+    private List<string[]> MusicList = new List<string[]>();
 
     void Start()
     {
-        // utf-8形式習得
-        encoding = Encoding.GetEncoding("utf-8");
         // csvFileを習得する
-        var csvFile = Application.streamingAssetsPath + ("Musics.csv");
+        string csvFilePath = Application.streamingAssetsPath + ("/Musics.csv");
+        // Debug.log確認
+        Debug.Log(csvFilePath);
+        string csvFile = File.ReadAllText(csvFilePath);
         // Debug.log確認
         Debug.Log(csvFile);
-        // Musics.csvの中身をutf-8に変更
-        var reader = File.ReadAllText();
+        // ToString変換を行いcsvFileの中身をString型に変換を行う
+        string stringcsv = csvFile.ToString();
+        Debug.Log(stringcsv);
 
-        // whileで一行ずつ習得
-        // 習得できなかった場合Peek値が-1になるので-1以上の時に習得する
-        while (reader.Peek() > -1)
-        {
-            // 1行ずつ読み込み
-            var lineRead = reader.ReadLine();
-            var Sep = lineRead.Split(',');
-            // 末尾まで繰り返す
-            musicList.Add(Sep);
+        string[] sep = stringcsv.Split('\n');
+        for (int i = 0; sep.Length <= i; i++)
+        { 
+        Debug.Log(sep[i].Split(','));
         }
-
-        //読み込んだデータを表示
-        foreach (var data in musicList)
-        {
-            Debug.Log("曲名:" + data[0] + "作曲:" + data[1]
-                      + "ノーツ作成者:" + data[2] + "カバーアルバム名" + data[3] + "ノーツデータ名:" + data[4]
-                      + "Easy:" + data[5] + "Normal:" + data[6] + "Hard:" + data[7] + "Pro:" + data[8]);
-        }
-    }
-
-    // Resources.Loadで実装していたため治すのに手こずり..作業途中です。
+        MusicList.Add(sep);    }
 }
