42dd0862a9d56e6fc706e9d88aac12b86575af7e
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-09-20 21:09:55 +0900

    演奏画面UI追加

--- Assets/Users/k.tamura/Scripts/NotesCountUp.cs
+++ Assets/Users/k.tamura/Scripts/NotesCountUp.cs
@@ -7,14 +7,14 @@ public class NotesCountUp : MonoBehaviour
 {
     [SerializeField] int gameType; 
 
-    GameObject obj;
-    ComboManager cm;
+    //GameObject obj;
+    //ComboManager cm;
 
-    private void Start()
-    {
-        obj = GameObject.Find("UICtrlCanvas");
-        cm = obj.GetComponent<ComboManager>();
-    }
+    //private void Start()
+    //{
+    //    obj = GameObject.Find("UICtrlCanvas");
+    //    cm = obj.GetComponent<ComboManager>();
+    //}
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Judge.NotesCountUp(this.gameObject.name, gameType);
