14fabd7bfd31a55abef88ffaf936f5ca1cdc9f10
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-09-25 00:17:07 +0900

    comboUI差し替え、バイオリン仮UI差し替え、PlayEndシーン追加

---
+++ Assets/Users/maekawa/Scripts/PlayEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEnd : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            SceneLoadManager.LoadScene("Title");
+        }
+    }
+}
