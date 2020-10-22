80138dcd393bfff803434006ae5651a90aab3fe5
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-10-22 14:26:38 +0900

    DrawJacket修正

--- Assets/Users/maekawa/Scripts/DrawJacket.cs
+++ Assets/Users/maekawa/Scripts/DrawJacket.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.iOS;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,13 +8,11 @@ public class DrawJacket : MonoBehaviour
     [SerializeField] Image jacket;
     [SerializeField] Text musicName;
 
-
     void Start()
     {
         Sprite[] jacketSprite = Resources.LoadAll<Sprite>("Jacket/");// ジャケットをすべて格納
         if (jacketSprite.Length > MusicDatas.MusicNumber)// 例外処理
         jacket.sprite = jacketSprite[MusicDatas.MusicNumber];// 曲１ = musicNum 0;
-
 
         musicName.text = MusicDatas.MusicName;
     }
