3be024e1305dabbcb14a5453b86c187cb98014b8
SakamakiDaiki <k4n1jp@yahoo.co.jp>
2020-10-30 18:21:08 +0900

    曲選択画面UI 難易度変更用スクリプト作成途中

---
+++ Assets/Users/sakamaki/Scripts/DiffNumber.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiffNumberManager : MonoBehaviour
+{
+    // 0 ~ 10 の数字を格納する用の宣言
+    [SerializeField] Sprite[] DiffNumber = new Sprite[10];
+    // 難易度の画像表示宣言
+    [SerializeField] Sprite DiffImage;
+    [SerializeField] Sprite[] Diff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
