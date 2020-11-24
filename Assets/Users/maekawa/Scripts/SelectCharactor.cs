56be22a5f0b3e324cb22fea2a97ca561235e2039
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-11-24 15:33:46 +0900

    キャラスワイプ制御途中

---
+++ Assets/Users/maekawa/Scripts/SelectCharactor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCharactor : MonoBehaviour
+{
+    [SerializeField] GameObject[] charaAnchor = new GameObject[3];
+
+    private float lastMousePosX;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, 10f, 1);
+
+        if(hit)
+        {
+            GameObject clickObj = hit.transform.gameObject;
+
+            if ((clickObj != null) && (clickObj.tag == "Right"))
+            {
+                float distance = lastMousePosX - Input.mousePosition.x;
+
+                Vector3 movePosition = new Vector3(distance, 0, 0);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    charaAnchor[i].transform.position += movePosition;
+                }
+
+                lastMousePosX = Input.mousePosition.x;
+            }
+        }
+    }
+}
