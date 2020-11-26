95ae5c0c210220b4ef16f5f56cb57aeab318ad97
maekawashota0606 <cs4700899@softbank.ne.jp>
2020-11-27 04:42:52 +0900

    キャラスワイプ仮

--- Assets/Users/maekawa/Scripts/SelectCharactor.cs
+++ Assets/Users/maekawa/Scripts/SelectCharactor.cs
@@ -4,32 +4,71 @@ using UnityEngine;
 
 public class SelectCharactor : MonoBehaviour
 {
-    [SerializeField] GameObject[] charaAnchor = new GameObject[3];
-
+    [SerializeField] GameObject[] charactor = new GameObject[3];
+    private Vector2[] charaPosition = new Vector2[3];
+    private float mousePositionX;
     private float lastMousePosX;
+    private bool isSwiping;
+
+    public static int gameType;
+
+    private void Start()
+    {
+        gameType = 0;
+
+        for (int i = 0; i < charactor.Length; i++)
+            charaPosition[i] = charactor[i].transform.position;
+    }
 
     private void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, 10f, 1);
 
-        if(hit)
+        if (Input.GetMouseButtonDown(0))
         {
-            GameObject clickObj = hit.transform.gameObject;
-
-            if ((clickObj != null) && (clickObj.tag == "Right"))
+            if ((hit.transform.gameObject != null) && (hit.transform.gameObject.tag == "Right"))
             {
-                float distance = lastMousePosX - Input.mousePosition.x;
-
-                Vector3 movePosition = new Vector3(distance, 0, 0);
-
-                for (int i = 0; i < 3; i++)
-                {
-                    charaAnchor[i].transform.position += movePosition;
-                }
-
                 lastMousePosX = Input.mousePosition.x;
+                isSwiping = true;
+            } 
+        }
+
+        if((Input.GetMouseButton(0)) && (isSwiping))
+        {
+            float distance = lastMousePosX - Input.mousePosition.x;
+
+            Vector3 movePosition = new Vector3(distance * 3, 0, 0);
+
+            for (int i = 0; i < charactor.Length; i++)
+            {
+                charactor[i].transform.position -= movePosition;
+            }
+
+            lastMousePosX = Input.mousePosition.x;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isSwiping = false;
+
+            if(charactor[0].transform.localPosition.x < 150)
+            {
+
+            }
+            else if(charactor[0].transform.localPosition.x > 850)
+            {
+
+            }
+            else
+            {
+                for(int i = 0; i < charactor.Length; i++)
+                    charactor[i].transform.position = charaPosition[i];
             }
         }
+    }
+
+    public void SetCharactor(int CharaNumber)
+    {
     }
 }
