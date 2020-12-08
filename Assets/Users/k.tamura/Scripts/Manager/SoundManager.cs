de51fdfea3c14f5674da4ffd2b281c3fb205984b
kuro-games <kuro@kuro.games>
2020-12-08 22:13:26 +0900

    コンパイル定数の付け加え

--- Assets/Users/k.tamura/Scripts/Manager/SoundManager.cs
+++ Assets/Users/k.tamura/Scripts/Manager/SoundManager.cs
@@ -7,8 +7,7 @@ using System;
 /// サウンドマネージャー
 /// singleton化済
 /// </summary>
-[Obsolete("現状ALLメモリー展開")]
-public class SoundManager:SingletonMonoBehaviour<SoundManager>
+public class SoundManager : SingletonMonoBehaviour<SoundManager>
 {
     [SerializeField]
     CriAtomSource BGMSource = default(CriAtomSource);
@@ -16,16 +15,6 @@ public class SoundManager:SingletonMonoBehaviour<SoundManager>
     CriAtomSource SESource = default(CriAtomSource);
     [SerializeField]
     CriAtomSource DemoBgmSource = default(CriAtomSource);
-#if false
-    /// <summary>
-    /// 非同期acbファイルロード用
-    /// </summary>
-    public static void BGMLoad()
-    {
-        
-    }
-
-#endif
     /// <summary>
     /// 初期化
     /// </summary>
@@ -33,22 +22,41 @@ public class SoundManager:SingletonMonoBehaviour<SoundManager>
     {
         DontDestroyOnLoad(this);
     }
+
     /// <summary>
     /// BGMを再生する関数 static化済
     /// </summary>
     /// <param name="cueID">cueID</param>
+#if SFAI_SOUND
+    public static void BGMSoundCue(string cueName)
+    {
+
+    }
+#else
     public static void BGMSoundCue(int cueID)
     {
         Instance.BGMSource.Play(cueID);
     }
+#endif
     /// <summary>
     /// SEを再生する関数 static化済
     /// </summary>
     /// <param name="cueID">cueID</param>
+#if SFAI_SOUND
+    public static void SESoundCue(string cueName)
+    {
+        
+    }
+#else
     public static void SESoundCue(int cueID)
     {
         Instance.SESource.Play(cueID);
     }
+#endif
+    /// <summary>
+    /// demo用BGMを流すファイル
+    /// </summary>
+    /// <param name="cueID"></param>
     public static void DemoBGMSoundCue(int cueID)
     {
         Instance.DemoBgmSource.Play(cueID);
@@ -61,6 +69,9 @@ public class SoundManager:SingletonMonoBehaviour<SoundManager>
         Instance.DemoBgmSource.Stop();
         Instance.BGMSource.Stop();
     }
+    /// <summary>
+    /// BGMを止めるためのプログラム
+    /// </summary>
     public static void BGMStop()
     {
         Instance.BGMSource.Stop();
@@ -73,6 +84,10 @@ public class SoundManager:SingletonMonoBehaviour<SoundManager>
     {
         return Instance.BGMSource.status;
     }
+    /// <summary>
+    /// BGMの再生時間の取得
+    /// </summary>
+    /// <param name="times"></param>
     public static void BgmTime(ref long times)
     {
         times = Instance.BGMSource.time;
