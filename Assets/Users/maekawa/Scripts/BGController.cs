2bfc920f005ceab369bf6ce65abda02f120efe1f
maekawashota0606 <cs4700899@softbank.ne.jp>
2021-02-08 14:59:39 +0900

    ピアノ、バイオリンシーンの背景を曲に応じて変更

---
+++ Assets/Users/maekawa/Scripts/BGController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGController : MonoBehaviour
+{
+    [SerializeField] Image backGround;
+
+    void Start()
+    {
+        Sprite[] jacketSprite = Resources.LoadAll<Sprite>("BackGround/");// ジャケットをすべて格納
+        if (jacketSprite.Length > MusicDatas.MusicNumber)// 例外処理
+            backGround.sprite = jacketSprite[MusicDatas.MusicNumber];// 曲１ = musicNum 0;
+    }
+}
