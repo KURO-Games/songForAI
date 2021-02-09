2dddf408dce8d33f931fdf6599288d1f8b58da0d
letconst <2000takeru93@gmail.com>
2021-02-10 08:26:47 +0900

    [Update] 演奏シーンのタップエフェクトスクリプトを抽象クラス化

---
+++ Assets/Users/Endo/Scripts/Effects/PianoPlayEffect.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PianoPlayEffect : PlayEffectBase
+{
+    [SerializeField]
+    private Animator[] holdEffect;
+
+    private bool[] _isHolding;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        _isHolding = new bool[MaxLaneNum];
+    }
+
+    protected override void PlayEffect()
+    {
+        for (int i = 0; i < MaxLaneNum; i++)
+        {
+            if (NotesJudgementBase.justTap[i])
+            {
+                tapEffect[i].SetBool(IsTapped, true);
+                tapEffect[i].Play("tapEffect", 0, 0);
+            }
+
+            if (NotesJudgementBase.isHold[i])
+            {
+                if (!_isHolding[i])
+                {
+                    holdEffect[i].SetBool(IsTapped, true);
+                    holdEffect[i].SetBool(IsTapped, false);
+                    holdEffect[i].Play("holdEffect", 0, 0);
+                    _isHolding[i] = true;
+                }
+            }
+            else if (_isHolding[i])
+            {
+                _isHolding[i] = false;
+                holdEffect[i].SetBool(IsEnded, true);
+            }
+        }
+
+        for (int i = 0; i < MaxLaneNum; i++)
+        {
+            NotesJudgementBase.justTap[i] = false;
+            tapEffect[i].SetBool(IsTapped, false);
+            holdEffect[i].SetBool(IsTapped, false);
+        }
+    }
+}
---
+++ Assets/Users/maekawa/Scripts/PlayEffectBase.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public abstract class PlayEffectBase : MonoBehaviour
+{
+    [SerializeField]
+    protected Animator[] tapEffect;
+
+    protected int MaxLaneNum;
+
+    protected static readonly int IsTapped = Animator.StringToHash("isTapped");
+    protected static readonly int IsEnded  = Animator.StringToHash("isEnded");
+
+    /// <summary>
+    /// エフェクトの再生処理
+    /// </summary>
+    protected abstract void PlayEffect();
+
+    protected virtual void Start()
+    {
+        MaxLaneNum = NotesGeneratorBase.MusicData.maxBlock;
+    }
+
+    private void Update()
+    {
+        PlayEffect();
+    }
+}
